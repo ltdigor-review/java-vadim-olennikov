6ceb9608a80af82dfc89fe47316db57333e6c755
ltdigor-review <ltdigor1@gmail.com>
2025-12-09 14:27:25 +0300

    Add checkstyle.xml

--- src/main/java/org/example/chapter01/task06/FactorialCalculator.java
+++ src/main/java/org/example/chapter01/task06/FactorialCalculator.java
@@ -3,10 +3,6 @@ package org.example.chapter01.task06;
 import java.math.BigInteger;
 
 public class FactorialCalculator {
-    private FactorialCalculator() {
-        throw new UnsupportedOperationException("Cannot instantiate utility class.");
-    }
-
     public static BigInteger factorial(int n) {
         BigInteger result = BigInteger.ONE;
         for (int i = 1; i <= n; i++) {
